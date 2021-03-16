216a3af8a6ed2ee57cf733391d9bc01af8960c24
Vincenzo <43069944+iamvincenzo@users.noreply.github.com>
2021-03-16 17:51:14 +0100

    Delete Assignement1 directory

--- Assignement1/doc/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"p":"<Unnamed>","l":"Activities"},{"p":"<Unnamed>","l":"Administrator"},{"l":"All Classes","u":"allclasses-index.html"},{"p":"<Unnamed>","l":"Club"},{"p":"<Unnamed>","l":"Competition"},{"p":"<Unnamed>","l":"Course"},{"p":"<Unnamed>","l":"ItemDemo"},{"p":"<Unnamed>","l":"Member"},{"p":"<Unnamed>","l":"People"}];updateSearchResults();
